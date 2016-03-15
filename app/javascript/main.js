33f53eebd4a95b4da07a7c13c6ab86377f053f53
MSturges <Sturges3232@gmail.com>
2016-03-14 21:01:20 -0600

    yay

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -11,7 +11,6 @@ var oPoster = $.ajax({
 
 ///split object into two arrays Item and Item price
 menuGetter.done(function(response){
-  console.log(response);
   var menu = response.menu
   var pizzaArray = [];
   var pizzaArrayPrice=[];
@@ -36,13 +35,13 @@ menuGetter.done(function(response){
 function appendPizza(pizzaArray, pizzaArrayPrice){
   var i = 0;
   $("#fSelL").append("<option class='menuTitle op' disabled>Pizza</option>")
-  for (var i = 0; i < pizzaArray.length; i++) {
+  $.each(pizzaArray, function(i, item){
     if(i===0){
       $("#fSelL").append("<option name='pizzaArray[i]' class='op' value="+pizzaArrayPrice[i]+" selected>"+pizzaArray[i]+"<option disabled name='pizzaArrayPrice[i]' class='menuPrice' value="+pizzaArrayPrice[i]+">$"+pizzaArrayPrice[i]+"</option>"+"</option>");
     }else{
       $("#fSelL").append("<option name='pizzaArray[i]' class='op' value="+pizzaArrayPrice[i]+">"+pizzaArray[i]+"<option disabled name='pizzaArrayPrice[i]' class='menuPrice' value="+pizzaArrayPrice[i]+">$"+pizzaArrayPrice[i]+"</option>"+"</option>")
     }
-  }
+  })
 };
 
 function appendBurgers(burgerArr, burgerArrPrice){
@@ -52,4 +51,28 @@ function appendBurgers(burgerArr, burgerArrPrice){
     $("#fSelL").append("<option name="+burgerArr[i]+" class='op' value="+burgerArrPrice[i]+">"+burgerArr[i]+"<option disabled name="+burgerArrPrice[i]+" class='menuPrice' value="+burgerArrPrice[i]+">$"+burgerArrPrice[i++]+"</option>"+"</option>")
 
   })
-}
+};
+
+//Math is Hard + more DOM Manipulation
+
+var sTotal = 0;
+$("#aitBtn").on('click',function(){
+  var count = $("#quantity").val();
+  if($("#quantity").val()>99 || $("#quantity").val()<1){
+    alert("Please enter positive quantity")
+  }else{
+    while (count>0) {
+      $("#ordersPlaced").append("<option class='op'>"+$("#fSelL :selected").html()+"<option class='opP'>$"+$("#fSelL :selected").val()+"</option></option>")
+      sTotal += parseFloat($("#fSelL").val());
+      count--
+    }
+  }
+  $("#area").html("Subtotal: $" + sTotal.toFixed(2)+"                  ")
+  $("#area").append("Sales Tax: 3.81%                  ")
+  $("#area").append("Total: $" + (sTotal+sTotal * .0381).toFixed(2));
+})
+$("#deliverIt").on('click', function(){
+  oPoster.done(function(response){
+    alert(response);
+  })
+})
